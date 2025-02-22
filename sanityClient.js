4181f0571b05f773457b4e46430e7c77c2c3baed
muhammedlayan12 <muhammedlayan12@gmail.com>
2025-02-23 01:47:14 +0500

    Fixing Issues

---
+++ sanityClient.js
@@ -0,0 +1,9 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "yftyosth", 
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2023-01-01",
+  token: "skdqx6yKv40RBVyiir52biOMPf129Bo78esITokTqLoGtxnwQ4OdOVhjrJUZFeZPVkaCDj55TDYrrHAzg00NHrblEF9fdrEcwl2nZHi4o81OCjRPKmupl2ACif0EyRsNSl3mIJqZDInTUzuVFjDFXlLhEQ3sDKcjNjPleh1ksghjhxl9sTYc", 
+});
